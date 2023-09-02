31ff69cfe652a2dbbb8791517126538a0125d46e
maershaa <valeria.work2022@gmail.com>
2023-09-02 12:36:32 +0300

    i need help

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,62 @@
+// ===== Создаем интерактивное веб-приложение, которое изменяет цвет
+//                  фона при нажатии на кнопки.           ===================== //
 
+// В этой части кода вы получаете ссылки на кнопки "Start" и "Stop" с помощью метода document.querySelector, используя атрибут [data-start] и [data-stop] для выбора элементов по атрибутам data. Также вы получаете ссылку на document.body, чтобы позже изменять цвет его фона.
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+// Создание переменной для хранения интервала:
+let timerId = null;
+
+//Функция генерации случайного цвета:
+// В этой функции вы создаете случайный шестнадцатеричный цвет, который будет использоваться как цвет фона для элемента body.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// Вы добавляете обработчики событий для кнопок "Start" и "Stop". Когда эти кнопки будут нажаты, будут вызываться соответствующие функции.
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+
+// Эта функция запускается при клике на кнопку "Start". Если интервал еще не запущен (!timerId), кнопка "Start" блокируется, и запускается интервал, который меняет цвет фона каждую секунду.
+function onStartButtonClick() {
+  if (!timerId) {
+    startButton.disabled = true;
+  }
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+// Эта функция вызывается при клике на кнопку "Stop". Она останавливает интервал (если он был запущен), сбрасывает значение timerId и разблокирует кнопку "Start".
+function onStopButtonClick() {
+  clearInterval(timerId);
+  timerId = null;
+  startButton.disabled = false;
+}
+
+// cтили
+// Задание размера кнопкам
+startButton.style.width = '150px';
+startButton.style.height = '50px';
+
+stopButton.style.width = '150px';
+stopButton.style.height = '50px';
+
+// задание размера тексту кнопок
+stopButton.style.fontSize = '24px';
+startButton.style.fontSize = '24px';
+
+// Расположение кнопок по центру экрана
+startButton.style.position = 'absolute';
+startButton.style.top = '50%';
+startButton.style.left = '40%';
+startButton.style.transform = 'translate(-50%, -50%)';
+
+stopButton.style.position = 'absolute';
+stopButton.style.top = '50%';
+stopButton.style.right = '40%';
+stopButton.style.transform = 'translate(-50%, -50%)';
---
+++ src/js/02-timer.js
@@ -0,0 +1,120 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const timer = document.querySelector('.timer');
+console.log(timer);
+const fields = document.querySelectorAll('.field');
+console.log('fields', fields);
+
+const values = document.querySelectorAll('.value');
+console.log('values', values);
+
+const labels = document.querySelectorAll('.label');
+console.log('labels', labels);
+
+const startButton = document.querySelector('[data-start]');
+console.log('startButton', startButton);
+
+const flatpickr = flatpickr('#datetime-picker');
+console.log('flatpickr', flatpickr);
+
+onChange();
+
+const date = new Date();
+// конечная дата
+let selectedDates;
+console.log('selectedDates', selectedDates);
+
+// текущая дата
+const currentDate = date.getTime();
+console.log(convertMs(currentDate));
+
+function onChange() {
+  startButton.isActive = false;
+}
+function onClose(selectedDates) {
+  selectedDates.forEach(selectedDate => {
+    if (selectedDate.getTime() < Date.now()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      startButton.isActive = true;
+    }
+  });
+}
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  // (callback), который будет выполнен, когда пользователь закроет календарь (например, после выбора даты и времени). Функция принимает аргумент selectedDates, который представляет выбранные пользователем даты и времена.
+  onClose,
+});
+
+// Для подсчета значений используй готовую функцию convertMs, где ms - разница между конечной и текущей датой в миллисекундах.
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+startButton.addEventListener('click', onClick);
+
+function onClick() {
+  const id = setInterval(() => {
+    result -= 1;
+    timer.textContent = result;
+    if (!result) {
+      clearInterval(id);
+    }
+  }, 1000);
+}
+
+function addLeadingZero(value) {
+  padStart();
+}
+
+// СТИЛИ
+// Применяем стили к элементу .timer
+timer.style.display = 'flex';
+timer.style.columnGap = '24px';
+
+// Применяем стили к каждому элементу .field
+fields.forEach(field => {
+  field.style.display = 'flex';
+  field.style.flexDirection = 'column';
+  field.style.flexWrap = 'wrap';
+  field.style.padding = '10px';
+  field.style.margin = '8px';
+  console.log('field', field);
+});
+
+values.forEach(value => {
+  // Применяем стили к элементу .value
+  value.style.fontWeight = '500';
+  value.style.fontSize = '40px';
+  console.log('value', value);
+});
+
+labels.forEach(label => {
+  // Применяем стили к элементу .label
+  label.style.textTransform = 'uppercase';
+  label.style.fontSize = '24px';
+  console.log('label', label);
+});
